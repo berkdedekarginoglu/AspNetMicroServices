882011ebe055b91d6e4732cad990590f33749a71
= <=>
2021-10-08 09:03:48 +0300

    Context-Inıtial

---
+++ src/Services/Catalog/Catalog.API/Entities/Product.cs
@@ -0,0 +1,20 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+
+namespace Catalog.API.Entities
+{
+    public class Product
+    {
+        [BsonId]
+        [BsonRepresentation(MongoDB.Bson.BsonType.ObjectId)]
+        public string Id { get; set; }
+        //Make this id column as a bson id in the mongo it will be generated from mongodb
+
+        [BsonElement("Name")]
+        public string Name { get; set; }
+        public string Category { get; set; }
+        public string Summary { get; set; }
+        public string Description { get; set; }
+        public string ImageFile { get; set; }
+        public decimal Price { get; set; }
+    }
+}
